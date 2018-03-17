ccccc16365bd186b39723fd0225cacbf8e5a63d8
Chery <chinrui1016@163.com>
2018-03-17 21:59:47 +0800

    zuul Api 网关

---
+++ springcloud-zuul/src/main/java/com/qchery/springcloud/zuul/ZuulApplication.java
@@ -0,0 +1,21 @@
+package com.qchery.springcloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Zuul 应用程序
+ *
+ * @author Chery
+ * @date 2018/3/17 21:38
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+}
